3729f3b3f83d2309d21858a6d0cb0ef0c08ff038
dextter1913 <Clau32usa@gmai.com>
2021-02-24 15:35:45 -0500

    Actualizacion

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>prcaticando conexion 2</title>
+</head>
+<body>
+    <center>
+    <h1>Practicando conexion 2</h1><br><br>
+    <form action="index.php" method="post"></form>
+    </center>
+</body>
+</html>
